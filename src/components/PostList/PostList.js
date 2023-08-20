42998020f46d3e527fbe9d0d1e30081810a29277
chrisIyer <hellohotbox420@gmail.com>
2023-08-20 19:03:48 -0400

    copying.

---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,23 @@
+import Post from '../Post/Post'
+import styles from './PostsList.module.scss'
+
+export default function PostsList ({ posts, deletePost, updatePost, user }) {
+  return (
+    <div className={styles.PostsList}>
+      <h3>Timeline</h3>
+      {/* <Post /> */}
+      <ul>
+        {posts.map(post => (
+          <Post
+            key={post._id}
+            post={post}
+            deletePost={deletePost}
+            updatePost={updatePost}
+            user={user}
+            
+          />
+        ))}
+      </ul>
+    </div>
+  )
+}
